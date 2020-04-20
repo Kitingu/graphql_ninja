956e5d14161e45b36bfaa4de2a3627c89b57f92d
kitingu <benlegendj@gmail.com>
2020-04-20 23:43:33 +0300

    setup graphql

--- server/app.js
+++ server/app.js
@@ -1,8 +1,21 @@
-const express = require('express')
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
 
-const app = express()
+const app = express();
 
-const PORT = 4000
-app.listen(PORT,()=>{
-    console.log('app running on port ${PORT}')
-})
+/**
+ * middleware for graphql ui
+ * type: function
+ * args: {
+ * schema: object,
+ * }
+ */
+
+app.use('/graphql',graphqlHTTP({
+
+}))
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log("app running on port ${PORT}");
+});
