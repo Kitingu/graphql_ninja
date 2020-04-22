49e7b6a97f47e60d28cd4f363205dabf00c704fd
kitingu <benlegendj@gmail.com>
2020-04-23 02:01:12 +0300

    update resolve functions

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -25,7 +25,7 @@ const BookType = new GraphQLObjectType({
       type: AuthorType,
       // parent is the book and contains an author id
       resolve(parent, args) {
-        // return _.find(authors, { id: parent.authorId });
+        return Author.findById(parent.authorId);
       },
     },
   }),
@@ -41,6 +41,7 @@ const AuthorType = new GraphQLObjectType({
       type: new GraphQLList(BookType),
       resolve(parent, args) {
         // return _.filter(books, { authorId: parent.id });
+        return Book.find({ authorId: parent.id });
       },
     },
   }),
@@ -98,7 +99,7 @@ const RootQuery = new GraphQLObjectType({
        */
       resolve(parent, args) {
         // code for getting data from db
-        // return _.find(books, { id: args.id });
+        return Book.findById(args.id);
       },
     },
     author: {
@@ -106,19 +107,20 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         // return _.find(authors, { id: args.id });
+        return Author.findById(args.id);
       },
     },
     books: {
       // get all books
       type: new GraphQLList(BookType),
       resolve(parent, args) {
-        return books;
+        return Book.find({});
       },
     },
     authors: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
-        return authors;
+        return Author.find({});
       },
     },
   },
