c7a9cdcc55d0f0efb9229d5c0833dbb8b86d5803
kitingu <benlegendj@gmail.com>
2020-04-23 20:01:49 +0300

    updating component state

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,38 +1,56 @@
 import React, { Component } from "react";
 import { graphql } from "react-apollo";
-import {getAuthorsQuery} from "../queries/queries"
+import { getAuthorsQuery } from "../queries/queries";
 
 class AddBook extends Component {
-
-    displayAuthors(){
-        let data = this.props.data;
-        if (data.loading) {
-          return <option disabled> Loading Authors..</option>;
-        } else {
-          return data.authors.map((author) => {
-            return <option key={author.id}> {author.name}</option>;
-          });
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+        name:"",
+        genre:"",
+        authorId:""
     }
-  render() {
+  }
+  displayAuthors() {
+    let data = this.props.data;
+    if (data.loading) {
+      return <option disabled> Loading Authors..</option>;
+    } else {
+      return data.authors.map((author) => {
+        return <option key={author.id}> {author.name}</option>;
+      });
+    }
+  }
+  submitForm(e){
+      e.preventDefault()
+      console.log(this.state);
+      
 
+  }
+  render() {
     return (
-      <form id="add-book">
+      <form id="add-book" onSubmit={this.submitForm.bind(this)}>
         <div className="field">
           <label htmlFor="book-name">Book Name:&nbsp;</label>
-          <input type="text" name="book-name" />
+          <input type="text" name="book-name" onChange={(e)=>this.setState({
+             name:e.target.value 
+          })} />
         </div>
 
         <div className="field">
           <label htmlFor="genre">Genre:&nbsp;</label>
-          <input type="text" name="genre" />
+          <input type="text" name="genre"  onChange={(e)=>this.setState({
+             genre:e.target.value 
+          })} />
         </div>
 
         <div className="field">
           <label htmlFor="author-name">Author</label>
-          <select name="author-name">
-              <option> Select Author</option>
-              {this.displayAuthors()}
+          <select onChange={(e)=>this.setState({
+             authorId:e.target.value 
+          })} name="author-name">
+            <option> Select Author</option>
+            {this.displayAuthors()}
           </select>
         </div>
 
