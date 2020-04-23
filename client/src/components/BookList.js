a1c3be3a8e7af44e08183362743b7024b2d1e660
kitingu <benlegendj@gmail.com>
2020-04-23 14:59:39 +0300

    renderering data in a component

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -11,15 +11,21 @@ const getBooksQuery = gql`
   }
 `;
 class BookList extends Component {
-   
+  //    graphql has passed in query results as props
+  displayBooks() {
+    let data = this.props.data;
+    if (data.loading) {
+      return <div> Loading Books</div>;
+    } else {
+      return data.books.map(book => {
+        return <li key={book.id}> {book.name}</li>;
+      });
+    }
+  }
   render() {
-
-     console.log(this.props);
     return (
       <div>
-        <ul id="book-list">
-          <li>Book</li>
-        </ul>
+        <ul id="book-list">{this.displayBooks()}</ul>
       </div>
     );
   }
